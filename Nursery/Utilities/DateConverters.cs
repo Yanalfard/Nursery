c94d6dcc179379ad8bbc18eeeb0a30cd439534ef
Lemi <Lemi@DESKTOP-SDD69VQ>
2021-05-20 15:42:52 +0430

    Admin Index

---
+++ Nursery/Utilities/DateConverters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nursery.Utilities
+{
+    public static class DateConverters
+    {
+        public static string ToRelativeDate(this DateTime date)
+        {
+            var dist = DateTime.Now.Subtract(date);
+            string ans = "";
+
+            if (TimeSpan.FromSeconds(0) <= dist && dist > TimeSpan.FromSeconds(60))
+            {
+                ans = $"{dist.Seconds} ثانیه";
+            }
+            else if (TimeSpan.FromMinutes(1) <= dist && dist > TimeSpan.FromMinutes(60))
+            {
+                ans = $"{dist.Minutes} دقیقه";
+            }
+            else if (TimeSpan.FromHours(1) <= dist && dist > TimeSpan.FromHours(24))
+            {
+                ans = $"{dist.Hours} ساعت";
+            }
+            else if (TimeSpan.FromDays(1) <= dist && dist > TimeSpan.FromDays(365))
+            {
+                ans = $"{dist.Days} روز";
+            }
+            else if (TimeSpan.FromDays(365) <= dist)
+            {
+                ans = $"{Math.Round((double)dist.Days / 365)} سال";
+            }
+
+            return ans;
+        }
+
+    }
+}
